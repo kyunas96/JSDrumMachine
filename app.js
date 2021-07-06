4f0743e87ad9b3c7ee461b97646b5ac615b749f7
Kevin Yunas <kevinyunas@icloud.com>
2021-07-06 08:35:06 -0700

    Working on 707 styling

--- app.js
+++ app.js
@@ -12,5 +12,11 @@ app.get('/', (req, res) => {
   // res.send("hello world");
 })
 
+app.get('/707', (req, res) => {
+  const toFile = path.resolve(__dirname, 'public', 'test.html')
+  res.sendFile(path.resolve(__dirname, 'public', '707.html'));
+  // res.send("hello world");
+})
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
---
+++ frontend/kits/js707.js
@@ -0,0 +1,78 @@
+import bd1 from "../../samples/707/707-bd1.wav";
+import bd2 from "../../samples/707/707-bd2.wav";
+import sd1 from "../../samples/707/707-sd1.wav";
+import sd2 from "../../samples/707/707-sd2.wav";
+import lt from "../../samples/707/707-lt.wav";
+import mt from "../../samples/707/707-mt.wav";
+import ht from "../../samples/707/707-ht.wav";
+import rs from "../../samples/707/707-rs.wav";
+import cb from "../../samples/707/707-cb.wav";
+import cp from "../../samples/707/707-cp.wav";
+import tb from "../../samples/707/707-tb.wav";
+import ch1 from "../../samples/707/707-ch1.wav";
+import ch2 from "../../samples/707/707-ch2.wav";
+import oh from "../../samples/707/707-oh.wav";
+import cr from "../../samples/707/707-cr.wav";
+import cy from "../../samples/707/707-cy.wav";
+
+const JS707Samples = [
+  bd1,
+  bd2,
+  sd1,
+  sd2,
+  lt,
+  mt,
+  ht,
+  rs,
+  cb,
+  cp,
+  tb,
+  ch1,
+  ch2,
+  oh,
+  cr,
+  cy,
+];
+
+const JS707Names = [
+  "bd1",
+  "bd2",
+  "sd1",
+  "sd2",
+  "lt",
+  "mt",
+  "ht",
+  "rs",
+  "cb",
+  "cp",
+  "tb",
+  "ch1",
+  "ch2",
+  "oh",
+  "cr",
+  "cy",
+];
+
+const JS707Keys = [
+  "a", // bd1
+  "q", // bd2
+  "s", // sd1
+  "w", // sd2
+  "d", // lt
+  "f", // mt
+  "g", // ht
+  "h", // rs
+  "j", // cb
+  "k", // cp
+  "l", // tb
+  ";", // ch1
+  "p", // ch2
+  "'", // cr
+  "[", //rd
+];
+
+export default {
+  samples: JS707Samples,
+  names: JS707Names,
+  keys: JS707Keys,
+};
