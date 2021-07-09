fd85fce984a448bfe99504995cabed1c8eb88349
Kevin Yunas <kevinyunas@icloud.com>
2021-07-09 16:04:33 -0700

    Setting up ssh

--- frontend/src/loadSamples.js
+++ frontend/src/loadSamples.js
@@ -4,46 +4,57 @@ export default function setup() {
 
   let bd = new Howl({
     src: ["./public/assets/samples/808-bd.wav"],
+    volume: 1.0
   });
 
   let sd = new Howl({
     src: ["./public/assets/samples/808-sd.wav"],
+    volume: 1.0,
   });
 
   let lt = new Howl({
     src: ["./public/assets/samples/808-lt.wav"],
+    volume: 1.0,
   });
 
   let mt = new Howl({
     src: ["./public/assets/samples/808-mt.wav"],
+    volume: 1.0,
   });
 
   let ht = new Howl({
     src: ["./public/assets/samples/808-ht.wav"],
+    volume: 1.0,
   });
 
   let rs = new Howl({
     src: ["./public/assets/samples/808-rs.wav"],
+    volume: 1.0,
   });
 
   let cp = new Howl({
     src: ["./public/assets/samples/808-cp.wav"],
+    volume: 1.0,
   });
 
   let cb = new Howl({
     src: ["./public/assets/samples/808-cb.wav"],
+    volume: 1.0,
   });
 
   let cy = new Howl({
     src: ["./public/assets/samples/808-cy.wav"],
+    volume: 1.0,
   });
 
   let oh = new Howl({
     src: ["./public/assets/samples/808-oh.wav"],
+    volume: 1.0,
   });
 
   let ch = new Howl({
     src: ["./public/assets/samples/808-ch.wav"],
+    volume: 1.0,
   });
 
   return {
