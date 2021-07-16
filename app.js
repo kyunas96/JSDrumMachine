7745c05e7a618b2cd97729a416074dabfba66433
Kevin Yunas <kevinyunas@icloud.com>
2021-07-16 13:01:05 -0700

    Finished up instructions

--- app.js
+++ app.js
@@ -12,11 +12,9 @@ app.get('/', (req, res) => {
   // res.send("hello world");
 })
 
-app.get('/707', (req, res) => {
-  const toFile = path.resolve(__dirname, 'public', 'test.html')
-  res.sendFile(path.resolve(__dirname, 'public', '707.html'));
-  // res.send("hello world");
-})
+app.get("*", function (req, res) {
+  res.redirect("/");
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
